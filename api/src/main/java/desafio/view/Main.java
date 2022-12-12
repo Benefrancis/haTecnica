9655b32dc5975b3e86733402bfa3f42d0a7dd12e
Benefrancis <benefrancis@gmail.com>
2022-12-12 15:11:24 -0300

    Transformando o projeto em Spring Boot

--- api/src/main/java/desafio/view/Main.java
+++ api/src/main/java/desafio/view/Main.java
@@ -1,24 +1,13 @@
 package desafio.view;
 
-import desafio.repository.ClienteCollectionRepository;
-import desafio.repository.EquipamentoCollectionRepository;
-import desafio.repository.ServicoCollectionRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
-        List<Collection> repos = Arrays.asList(
-
-//                ServicoCollectionRepository.findAll(),
-//                EquipamentoCollectionRepository.findAll(),
-                ClienteCollectionRepository.findAll());
-
-        repos.forEach(
-                r -> r.forEach(System.out::println)
-        );
+        SpringApplication.run(Main.class, args);
     }
+
 }
